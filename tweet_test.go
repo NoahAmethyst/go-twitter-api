52de05973439f35fa1d118a8cb19075b61ffd3e5
Amethyst <zhonghaoma@blackfish.cn>
2022-12-20 14:50:13 +0800

    -- add tweets api

--- tweet_test.go
+++ tweet_test.go
@@ -45,11 +45,12 @@ func TestGetTweets(t *testing.T) {
 
 	cli := NewClient(twitterConsumeKey, twitterConsumerSecret)
 
-	tweet, err := cli.GetTweet(TestTweetId)
+	tweets, err := cli.GetTweets([]int64{1604578908124450816, 1604862158843645952, 1604862398032056320})
 	if err != nil {
 		t.Error(err.Error())
 		return
 	}
-
-	t.Logf("result:%v", tweet)
+	for _, _tweet := range tweets.Data {
+		t.Logf("%v", _tweet)
+	}
 }
